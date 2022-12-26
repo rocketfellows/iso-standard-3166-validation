d77e49b886e597fc7005af90b422584d5284ffd2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-12-27 01:53:04 +0400

    Added alpha2 validator.

---
+++ tests/unit/validators/Alpha2Test.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace rocketfellows\ISOStandard3166Validation\tests\unit\validators;
+
+use rocketfellows\ISOStandard3166Validation\validators\Alpha2;
+
+/**
+ * @group validators
+ */
+class Alpha2Test extends ValidatorTest
+{
+    public function getAlpha2CodesAssertionData(): array
+    {
+        return [
+            'valid RU alpha2 code' => [
+                'alpha2' => 'RU',
+                'isValid' => true,
+            ],
+            'valid DE alpha2 code' => [
+                'alpha2' => 'DE',
+                'isValid' => true,
+            ],
+            'invalid PP code' => [
+                'alpha2' => 'PP',
+                'isValid' => false,
+            ],
+            'invalid RUS code' => [
+                'alpha2' => 'RUS',
+                'isValid' => false,
+            ],
+        ];
+    }
+
+    protected function getValidatorClass(): string
+    {
+        return Alpha2::class;
+    }
+}
