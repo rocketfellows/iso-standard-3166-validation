5b776ca54f5b4869e68f20f5b390a68cdeb29d7f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-12-28 01:38:46 +0400

    Covered alpha3 batch

---
+++ tests/unit/validators/batch/Alpha3BatchTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace rocketfellows\ISOStandard3166Validation\tests\unit\validators\batch;
+
+/**
+ * @group batch-validators
+ */
+class Alpha3BatchTest extends BatchValidatorTest
+{
+    protected function getValidatorClassName(): string
+    {
+        return Alpha3Batch::class;
+    }
+
+    public function getAssertionData(): array
+    {
+        return [
+            'all values valid' => [
+                'values' => ['RUS', 'DEU', 'GBR',],
+                'expectedResult' => null,
+            ],
+            'one value invalid' => [
+                'values' => ['RUS', 'DEU', 'foo',],
+                'expectedResult' => ['foo',],
+            ],
+            'multiple values invalid' => [
+                'values' => ['RUS', 'DEU', 'foo', 'bar',],
+                'expectedResult' => ['foo', 'bar'],
+            ],
+            'nothing to validate' => [
+                'values' => [],
+                'expectedResult' => null,
+            ],
+        ];
+    }
+}
